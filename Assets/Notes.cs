3b348ff51deae2520cf328657c43a08d642c4b45
Shalin Lathigra <shalinlathigra@gmail.com>
2020-06-06 03:09:34 -0400

    [NOTES] Next Steps
    Signed-off-by: Shalin Lathigra <shalinlathigra@gmail.com>

---
+++ Assets/Notes.cs
@@ -0,0 +1,6 @@
+﻿/************************************************
+Next Step:
+
+	Need to clear area underneath Solid objects at startup
+	Non-Solid objects need to turn invisible when you go underneath them
+************************************************/
