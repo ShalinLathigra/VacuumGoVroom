7d1b9a2571e18eaa2f76d65ee47cb7b9f995b657
EDM <terrapetalousa@gmail.com>
2020-06-09 18:30:30 -0400

    Update notes

--- Assets/Notes.cs
+++ Assets/Notes.cs
@@ -15,4 +15,8 @@ Next Step:
 	[5][CORE] Design & Implement Dog algorithms with placeholder model
 
 	[4][FLUFF] Add Dust particles/glowy dots to god rays
+
+	[6][CORE] Start in Main Menu, select button to start game
+	[7][CORE] Play noise when roomba is vacuuming
+
 ************************************************/
