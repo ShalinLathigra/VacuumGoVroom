416359d3350ad0ea9df3754af60423f5ce8d1a1b
EDM <terrapetalousa@gmail.com>
2020-06-09 21:03:14 -0400

    [7][CORE] Added main menu scene, set up default scene in build settings Signed-off-by: EDM <terrapetalousa@gmail.com>

---
+++ Assets/InfiniteRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteRotate : MonoBehaviour
+{
+
+    float smooth = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        transform.rotation *= Quaternion.AngleAxis(Time.deltaTime * smooth, Vector3.up);
+    }
+}
