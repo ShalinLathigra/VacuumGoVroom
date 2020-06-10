72df035d11ae2d935ee57ad755e48a2409635e99
Shalin Lathigra <shalinlathigra@gmail.com>
2020-06-09 20:50:05 -0400

    Add Preliminary Schedule
    Signed-off-by: Shalin Lathigra <shalinlathigra@gmail.com>

--- Assets/Notes.cs
+++ Assets/Notes.cs
@@ -11,8 +11,32 @@ Next Step:
 
 	[1][CORE] Clear area underneath Solid objects at startup	//Done
 	[2][CORE] Non-Solid objects need to turn invisible when you go underneath them	//Done
+
 	[3][CORE] Improve Camera motion fluidity	//In Progress
-	[5][CORE] Design & Implement Dog algorithms with placeholder model
 
 	[4][FLUFF] Add Dust particles/glowy dots to god rays
+	[5][CORE] Design & Implement Dog algorithms with placeholder model
+
+	git commit --signoff
+
+*************************************************
+
+	Core Game mechanics - June 30th
+	
+	Particles/Shader fun times - July 14th
+	Asset Acquisition - July 14th
+	Audio Design - July 14th
+	
+	Level Design - July 30th
+	
+	User Interface/Menus - August 7th
+
+	Testing/Refining - August 20th
+
+
+	Feature Freeze July 31st
+	Polish only until August 20th
+	Release it somewhere on August 21st
+		Itch.io
+
 ************************************************/
